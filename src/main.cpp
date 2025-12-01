a40532da6e5c49377458daf7ef15b66275e9a5f8
David Wedel <davidwedel06@gmail.com>
2025-12-01 17:57:43 -0600

    works correctly -- I think

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@ int prevPinState;
 int pulseCounter = 0;
 int eggsPerCycle = 6;
 
-//pins
-const int sensorPin = A0;
+// pins
+const int sensorPin = A5;
 const int pulsePin = 2;
 const int ledPin = 13;
 
@@ -13,58 +13,60 @@ const int ledPin = 13;
 unsigned long prevCheck = 0;
 unsigned long prevBlink;
 
-//intervals
+// intervals
 const unsigned long checkInterval = 50;
 const unsigned long blinkInterval = 200;
 
 int pulseState = LOW;
 
-
-
 void setup() {
-  pinMode(sensorPin, INPUT);  
+  pinMode(sensorPin, INPUT);
   pinMode(pulsePin, OUTPUT);
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
+  prevPinState = analogRead(sensorPin);
 }
 
 void loop() {
-	unsigned long now = millis();
+  unsigned long now = millis();
 
-	//check input every 10 ms
-	if(now - prevCheck >= checkInterval) {
-		prevCheck = now;
-		int pinState = analogRead(sensorPin);
-		Serial.println(pinState);
+  // check input every 10 ms
+  if (now - prevCheck >= checkInterval) {
+    prevCheck = now;
+    int pinState = analogRead(sensorPin);
+    // Serial.println(pinState);
 
-		// difference greater than 600
-		if (prevPinState - pinState > 600) {
-			pulseCounter += eggsPerCycle;
-			Serial.println(eggsPerCycle);
-			prevPinState = pinState;
-		}
+    // difference greater than 600
+    int diff = prevPinState - pinState;
+    Serial.print("diff");
+    Serial.println(diff);
+    if (diff > 600) {
+      pulseCounter += eggsPerCycle;
+      Serial.println(eggsPerCycle);
+    }
+    prevPinState = pinState;
 
-		// shows status of beam. High beam broken, Low beam shining through
-		if (1023 - pinState < 800) {
-			digitalWrite(ledPin, HIGH);
-		} else {
-			digitalWrite(ledPin, LOW);
-		}
-	}
+    // shows status of beam. High beam broken, Low beam shining through
+    if (1023 - pinState < 800) {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
+  }
 
-	// Handle pulsing 
-	if (pulseCounter > 0 && (now - prevBlink >= blinkInterval)) {
-		prevBlink = now;
+  // Handle pulsing
+  if (pulseCounter > 0 && (now - prevBlink >= blinkInterval)) {
+    prevBlink = now;
 
-		//Toggle counter on rotem
-		pulseState = !pulseState;
+    // Toggle counter on rotem
+    pulseState = !pulseState;
 
-		digitalWrite(pulsePin, pulseState);
+    digitalWrite(pulsePin, pulseState);
 
-		if(pulseState == LOW){
-			pulseCounter--;
-		}
-	}
+    if (pulseState == LOW) {
+      pulseCounter--;
+    }
+  }
 
-	delay(1);
+  delay(1);
 }
